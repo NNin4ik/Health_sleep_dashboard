da4114ed2243d6abc662cfd70cf381bb61655fcc
NNin4ik <ninevia03@gmail.com>
2024-06-24 00:38:48 +0300

    Добавление ссылки

--- pages/main.py
+++ pages/main.py
@@ -63,7 +63,7 @@ layout = dbc.Container([
 
     dbc.Row([
         dbc.Col([
-            html.P(html.A("Реализацию готового проекта можно посмотреть здесь", href="#", className="text-decoration-underline fs-5"))
+            html.P(html.A("Реализацию готового проекта можно посмотреть здесь", href="https://github.com/NNin4ik/Health_sleep_dashboard", className="text-decoration-underline fs-5"))
         ], width=8),
 
         dbc.Col([
